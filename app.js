1f8d6d57893782794cf9a43c7879549fd850c27c
Aram2142 <andresaaramirez@aol.com>
2021-01-18 23:03:39 -0500

    remade script

--- app.js
+++ app.js
@@ -1,55 +1,93 @@
-//You are going to write program, not a web page , only pure javascript, it’s a guessing game . 
-//You re program should should run until the user don’t want to play anymore. 
-//So you going to have a random number from 1-10 , 
-//if the user don’t guess the number in 3 tries then he or she loses and you should ask the user 
-//if they want to play again if yes keep playing other wise just kill the program
+//NODE version
+/*Logic modified from peterjacobson guessing game on github*/
+//npm installs
+const readline = require('readline');
+const Confirm = require('prompt-confirm');
+const prompt = new Confirm("Want to play again?");
 
-//You are going to write program, not a web page , only pure javascript, it’s a guessing game . 
-//You re program should should run until the user don’t want to play anymore. 
-//So you going to have a random number from 1-10 , 
-//if the user don’t guess the number in 3 tries then he or she loses and you should ask the user 
-//if they want to play again if yes keep playing other wise just kill the program
+const question = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+//generates random number between 1 and 10
+let randomNum = Math.floor(Math.random() * 10) + 1;
+var attempts = 3;
 
-// var computerNum;
-
-// //variables to hold wins and losses. Start at 0
-// var wins = 0;
-// var losses = 0;
-
-var randomNum = Math.floor(Math.random() * 10) + 1;
-function guessNumber() {
-    var userGuess = parseInt(prompt("I'm thinking of a number between 1 and 10, what is it?"));
+question.setPrompt("I am thinking of a number between 1 and 10, what is it?");
+question.prompt();
+question.on('line', function (guess) {
+    var userGuess = parseInt(guess);
 
     //logic to check which are correct and not correct
     if (userGuess < randomNum) {
-        alert("your guess is too low");
-    }else if(userGuess > randomNum) {
-        alert("your guess is too high");
-    }else {
-        alert("CORRECT!");
-        return; //this is to break that loop
+        console.log("your guess is too low");
+        console.log("you have " + attempts + " attempts left")
+    } else if (userGuess > randomNum) {
+        console.log("your guess is too high");
+        console.log("You have " + attempts + " attempts left")
+    } else {
+        console.log("CORRECT!");
+        process.exit(0);
     }
-    
 
-}
-for (var i = 0; i < 10; i++) {
-    //If it is after the user's 3rd or 6th turn
-    if (i == 3 || i == 6 || i == 9) {
-        if (!confirm("Do you still want to keep playing?")) {
-            break;
-        }
+    attempts--
+    if (attempts == 0) {
+        console.log("GAME OVER");
+        console.log("You have no attempts left");
+        process.exit(0);
     }
-    guessNumber(randomNum);
-}
 
-    // if((userGuess < randomNum) && (numOfGuesses == 1)) {
-    //     alert("no")
-    // } else if ((userGuess > randomNum) && (numOfGuesses == 2)) {
-    //     //alert("nope")
-    // } else if(numOfGuesses == 3) {
- 
-    //     var end = prompt("press any key to continue?")
-        
+    question.prompt()
+
+});
+
+//guessNumber(randomNum)
+    // for (var i = 0; i < 10; i++) {
+    //     //If it is after the user's 3rd or 6th turn
+    //     if (userGuess == randomNum) {
+    //         const prompt = new Confirm("Would you like to keep playing?");
+    //         prompt.ask(function (answer) {
+    //             console.log(answer);
+    //         })
+    //     }
+    //     //guessNumber(randomNum);
     // }
-    
+
+
+
+
+
+
+
+
+
+
+
+
+// WEB VERSION
+// var randomNum = Math.floor(Math.random() * 10) + 1;
+// function guessNumber() {
+//     var userGuess = parseInt(prompt("I'm thinking of a number between 1 and 10, what is it?"));
+
+//     //logic to check which are correct and not correct
+//     if (userGuess < randomNum) {
+//         alert("your guess is too low");
+//     }else if(userGuess > randomNum) {
+//         alert("your guess is too high");
+//     }else {
+//         alert("CORRECT!");
+//         return; //this is to break that loop
+//     }
+
+
+// }
+// for (var i = 0; i < 10; i++) {
+//     //If it is after the user's 3rd or 6th turn
+//     if (i == 3 || i == 6 || i == 9) {
+//         if (!confirm("Do you still want to keep playing?")) {
+//             break;
+//         }
+//     }
+//     guessNumber(randomNum);
+// }
